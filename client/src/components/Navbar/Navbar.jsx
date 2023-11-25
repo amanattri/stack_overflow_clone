cbe4405e36129e5d587ebfb3fc447a899c1a3d41
amanattri <138004474+amanattri@users.noreply.github.com>
2023-11-26 01:50:07 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useCallback  } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { InvalidTokenError, jwtDecode } from 'jwt-decode';
+import {jwtDecode } from 'jwt-decode';
 
 import logo from "../../assets/logo.png";
 import search from "../../assets/search-solid.svg";
@@ -15,11 +15,12 @@ const Navbar = ({ handleSlideIn }) => {
   var User = useSelector((state) => state.currentUserReducer);
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  const handleLogout = useCallback(() => {
     dispatch({ type: "LOGOUT" });
     navigate("/");
     dispatch(setCurrentUser(null));
-  };
+  }, [dispatch, navigate]);
+
 
   useEffect(() => {
     const token = User?.token;
@@ -30,7 +31,7 @@ const Navbar = ({ handleSlideIn }) => {
       }
     }
     dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
-  }, [User?.token, dispatch]);
+  }, [User?.token, dispatch,handleLogout]);
 
   return (
     <nav className="main-nav">
